d67f41aa5c970f0fd811f93314db70fd23ce713c
Andres Haro <hvaandres@icloud.com>
2020-10-21 23:20:00 -0600

    First Post

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'Insert_Consumer_Key',
+    consumer_secret: 'Insert_Consumer_Secret',
+    access_token:'Insert_Consumer_Token',
+    access_token_secret: 'Insert_Access_Token_Secret'
+}
